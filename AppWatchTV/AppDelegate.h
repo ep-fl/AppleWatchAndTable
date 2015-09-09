761335ae9fc1fffe2930e25c7521132ea20edac6
ep_fl <socialinfo.my@gmail.com>
2015-09-09 15:03:30 +0300

    Initial Commit

---
+++ AppWatchTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppWatchTV
+//
+//  Created by fl on 09.09.15.
+//  Copyright (c) 2015 LWO LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
